1ee123fafd442dcd82bace07bed263a681f348a6
Izhan Sastre Hernando <izhansastre@gmail.com>
2025-03-12 13:19:06 +0100

    clean up main

--- main.c
+++ main.c
@@ -6,65 +6,44 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 14:20:21 by isastre-          #+#    #+#             */
-/*   Updated: 2025/03/11 22:49:47 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/03/12 13:18:27 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
-int main(int argc, char *argv[])
+/**
+ * @brief inits stacks and populates stack_a with the program input
+ * @returns 1 if no input params
+ * 			0 if program execs till the end
+ */
+int	main(int argc, char *argv[])
 {
-	if (argc == 1)
-		return (1); // error: no input params
-	if (argc == 2)
-	{
-		argv = ft_split(argv[1], ' ');
-		printf("splitted\n");
-		
-		// while (*argv)
-		// {
-		// 	printf("<%s>\n", *argv);
-		// 	argv++;
-		// }
-		
-	}
-	if (argc > 2) // TODO cambiar por else
-		argv++; // para saltar el nombre del programa
-	
-	int n;
-	t_stack *stack_a;
-	t_stack *stack_b;
+	int		n;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 
+	if (argc == 1)
+		return (1);
+	argv++;
+	if (argc == 2)
+		argv = ft_split(*argv, ' ');
+	
+	// stacks init
 	stack_a = malloc(sizeof(t_stack));
 	stack_b = malloc(sizeof(t_stack));
 	stack_b->head = NULL;
 	stack_b->tail = NULL;
 	
+	// populate stack_a
 	while (*argv)
 	{
-		n = ft_atoi(*argv, stack_a);
+		n = ft_atoi(*argv, stack_a); // ! n no se esta usando
 		argv++;
 	}
 
-	// PRUEBAS swap
-	ft_new_node(stack_b, 4);
-	// ft_new_node(stack_b, 5);
-	// ft_new_node(stack_b, 6);
-
-	ft_print_stack(stack_a, 'a');
-	ft_print_stack(stack_b, 'b');
-	ft_pa(stack_a, stack_b);
-	ft_print_stack(stack_a, 'a');
-	ft_print_stack(stack_b, 'b');
-	printf("--------------------------------------\n");
-
-	ft_print_stack(stack_a, 'a');
-	ft_print_stack(stack_b, 'b');
-	ft_pb(stack_a, stack_b);
 	ft_print_stack(stack_a, 'a');
 	ft_print_stack(stack_b, 'b');
 	
-	return 0;
+	return (0);
 }
-
