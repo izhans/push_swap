7c24121744f760ec446959416e8a8def4aa12dec
Izhan Sastre Hernando <izhansastre@gmail.com>
2025-03-12 13:06:24 +0100

    clean up + norminette .h

--- push_swap.h
+++ push_swap.h
@@ -6,67 +6,59 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 09:24:22 by isastre-          #+#    #+#             */
-/*   Updated: 2025/03/11 22:45:33 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:55:02 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h> // TODO delete
 
 #ifndef PUSH_SWAP_H
 
 # define PUSH_SWAP_H
+// ##### includes #####
+# include <stdio.h> // TODO delete
+// utils
+# include <stdlib.h>
+# include <limits.h>
 
-typedef struct node
+// ##### structs #####
+typedef struct s_node
 {
-	struct node	*prev;
-	struct node	*next;
-	int			value;
-} t_node;
+	struct s_node	*prev;
+	struct s_node	*next;
+	int				value;
+}	t_node;
 
-
-typedef struct stack
+typedef struct s_stack
 {
-	t_node *head;
-	t_node *tail;
-} t_stack;
-
+	t_node	*head;
+	t_node	*tail;
+}	t_stack;
 
 // ##### movs (pa, ra, ...) #####
 // push
-void ft_pa(t_stack *stack_a, t_stack *stack_b);
-void ft_pb(t_stack *stack_a, t_stack *stack_b);
+void	ft_pa(t_stack *stack_a, t_stack *stack_b);
+void	ft_pb(t_stack *stack_a, t_stack *stack_b);
 // swap
-void ft_sa(t_stack *stack_a);
-void ft_sb(t_stack *stack_b);
-void ft_ss(t_stack *stack_a, t_stack *stack_b);
+void	ft_sa(t_stack *stack_a);
+void	ft_sb(t_stack *stack_b);
+void	ft_ss(t_stack *stack_a, t_stack *stack_b);
 // rotate
-void ft_ra(t_stack *stack_a);
-void ft_rb(t_stack *stack_b);
-void ft_rr(t_stack *stack_a, t_stack *stack_b);
+void	ft_ra(t_stack *stack_a);
+void	ft_rb(t_stack *stack_b);
+void	ft_rr(t_stack *stack_a, t_stack *stack_b);
 // reverse rotate
-void ft_rra(t_stack *stack_a);
-void ft_rrb(t_stack *stack_b);
-void ft_rrr(t_stack *stack_a, t_stack *stack_b);
+void	ft_rra(t_stack *stack_a);
+void	ft_rrb(t_stack *stack_b);
+void	ft_rrr(t_stack *stack_a, t_stack *stack_b);
 
-// utils
-#include <stdlib.h>
-#include <limits.h>
-
-void ft_exit();
-int	ft_value_exists(t_stack *stack, int value);
-void ft_new_node(t_stack *stack, int value);
-
+// ##### utils #####
+void	ft_exit(void);
+// stack utils
+int		ft_value_exists(t_stack *stack, int value);
+void	ft_new_node(t_stack *stack, int value);
+void	ft_print_stack(t_stack *stack, char c);
+// libft utils
 char	**ft_split(char const *s, char c);
-int	ft_atoi(char *str, t_stack *stack);
+int		ft_atoi(char *str, t_stack *stack);
 size_t	ft_strlen(const char *s);
-
-void ft_print_stack(t_stack *stack, char c);
-
-/*
-void	ft_swap(t_node *stack);
-void	ft_push(t_node *src_stack, t_node *dest_stack);
-// rotate & rev rotate -> pasar head y tail en vez de stack
-void	ft_reverse_rotate(t_node *stack);
-*/
 
 #endif
