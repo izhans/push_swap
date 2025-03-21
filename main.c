d6e3daa6864ac000ca3471a28ee58898f334903a
izhans <izhansastre@gmail.com>
2025-03-21 02:15:16 +0100

    refactored, solved leaks and norminetted main.c

--- main.c
+++ main.c
@@ -6,15 +6,18 @@
 /*   By: isastre- <isastre-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 14:20:21 by isastre-          #+#    #+#             */
-/*   Updated: 2025/03/19 22:06:01 by isastre-         ###   ########.fr       */
+/*   Updated: 2025/03/21 02:12:30 by isastre-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_init_stack_a(char *argv[], int split, t_stack **stack_a);
+void	ft_exec_algorithm(t_stack *stack_a, t_stack *stack_b);
+
 /**
  * @brief 	creates stacks, populates stack_a with the program input,
- * 			frees stacks at the end
+ * 			selects sort and frees stacks at the end
  * @returns 1 if no input params
  * 			0 if program execs till the end
  */
@@ -26,36 +29,56 @@ int	main(int argc, char *argv[])
 	if (argc == 1)
 		return (1);
 	argv++;
-	if (argc == 2)
-		argv = ft_split(*argv, ' '); // TODO hay que liberar esta memoria
-	
-	// create stack_a
-	stack_a = malloc(sizeof(t_stack));
-	stack_a->head = NULL;
-	stack_a->tail = NULL;
-	
-	// populate stack_a
-	while (*argv)
-	{
-		ft_atoi(*argv, stack_a);
-		argv++;
-	}
-
+	ft_init_stack_a(argv, argc == 2, &stack_a);
 	if (ft_stack_is_sorted(stack_a))
 	{
 		ft_delete_stack(&stack_a);
 		return (0);
 	}
-	
-	// create stack_b
 	stack_b = malloc(sizeof(t_stack));
+	if (!stack_b)
+		return (ft_free_and_exit(stack_a), 1);
 	stack_b->head = NULL;
 	stack_b->tail = NULL;
+	ft_exec_algorithm(stack_a, stack_b);
+	ft_delete_stack(&stack_a);
+	ft_delete_stack(&stack_b);
+	return (0);
+}
 
-	ft_print_stack(stack_a, 'a');
-	ft_print_stack(stack_b, 'b');
+/**
+ * creates and populates stack_a
+ */
+void	ft_init_stack_a(char *argv[], int split, t_stack **stack_a)
+{
+	char	**splited;
 
-	unsigned int	numbers = ft_stack_size(stack_a);
+	if (split)
+	{
+		splited = ft_split(*argv, ' ');
+		if (*splited == NULL)
+			ft_exit();
+		argv = splited;
+	}
+	*stack_a = malloc(sizeof(t_stack));
+	if (!*stack_a)
+		return (ft_free_and_exit(*stack_a));
+	(*stack_a)->head = NULL;
+	(*stack_a)->tail = NULL;
+	while (*argv)
+	{
+		ft_atoi(*argv, *stack_a);
+		argv++;
+	}
+	if (split)
+		free(splited);
+}
+
+void	ft_exec_algorithm(t_stack *stack_a, t_stack *stack_b)
+{
+	unsigned int	numbers;
+
+	numbers = ft_stack_size(stack_a);
 	if (numbers == 2)
 		ft_sa(stack_a);
 	else if (numbers == 3)
@@ -66,11 +89,4 @@ int	main(int argc, char *argv[])
 		ft_sort_five(stack_a, stack_b);
 	else
 		ft_sort(stack_a, stack_b);
-		
-	ft_print_stack(stack_a, 'a');
-	ft_print_stack(stack_b, 'b');
-	// free stacks
-	ft_delete_stack(&stack_a);
-	ft_delete_stack(&stack_b);
-	return (0);
 }
